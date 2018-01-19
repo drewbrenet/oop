1eb5523a9eb179f8b4cad6dc654871ce1ad5e659
drew-brenet.oispuu <drewoispuu@ikt.khk.ee>
2018-01-19 09:50:47 +0200

    kaustade ja failide konstantsed nimetused

---
+++ READEME.md
@@ -0,0 +1,8 @@
+# Programmeerimisvahendite kasutamine
+
+"Programmeerimisvahendite kasutamie"
+
+##Alustamiseks
+All on lisatud läsklused ja kirjeldused, mis on vaja jälgida rakenduse koostamisel ja tööle panemisel
+
+###
---
+++ conf.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: drew-brenet.oispuu
+ * Date: 19.01.2018
+ * Time: 9:45
+ */
+
+// kaustade ja failide konstantsed nimetused
+define('MODEL_DIR', 'model/');
+define('VIEW_DIR', 'views/');
+define('CONTROL_DIR', 'controllers/');
