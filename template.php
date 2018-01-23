bbffbb256247f79b1400b3cc455eab3671235b81
drew-brenet.oispuu <drewoispuu@ikt.khk.ee>
2018-01-23 10:48:53 +0200

    Malli täitmine reaalväärtustega ja kasutamine vaade väljundis

--- template.php
+++ template.php
@@ -7,45 +7,39 @@
  */
 class template
 {
-    // klassi muutujad
-    var $file = ''; // HTML malli faili nimi
-    var $content = false; // HTML malli failist loetud sisu
-    var $vars = array(); // HTML malli elementide nimetuste ja reaalväärtuste paarid
-    /**
-     * template constructor.
-     * @param string $file
-     */
+  
+    var $file = '';
+    var $content = false;
+    var $vars = array();
+
     public function __construct($file)
     {
-        $this->file = $file; // määrame kasutatava malli faili nimi
-        $this->loadFile(); // laadime määratud nimega faili sisu
+        $this->file = $file;
+        $this->loadFile();
     }
-    // HTML malli faili nime ja õiguste kontroll
-    // ning sisu lugemine siis, kui vajalikud
-    // tingimused on täidetud
+
     function loadFile(){
         if(!is_dir(VIEW_DIR)){
             echo 'Ei ole leitud '.VIEW_DIR.' kataloogi<br />';
             exit;
         }
 
-        $file = $this->file;
+        $file = $this->file; // abiasendus
         if(file_exists($file) and is_file($file) and is_readable($file)){
             $this->readFile($file);
         }
 
-
-        $file = VIEW_DIR.$this->file;
+        $file = VIEW_DIR.$this->file; // abiasendus
         if(file_exists($file) and is_file($file) and is_readable($file)){
             $this->readFile($file);
         }
 
-        $file = VIEW_DIR.$this->file.'.html';
+        $file = VIEW_DIR.$this->file.'.html'; // abiasendus
         if(file_exists($file) and is_file($file) and is_readable($file)){
             $this->readFile($file);
         }
 
-        $file = VIEW_DIR.str_replace('.', '/', $this->file).'.html';
+        $file = VIEW_DIR.str_replace('.', '/', $this->file).'.html'; // abiasendus
         if(file_exists($file) and is_file($file) and is_readable($file)){
             $this->readFile($file);
         }
@@ -60,14 +54,15 @@ class template
 
         $this->content = file_get_contents($file);
     }
-    function set($name, $value) {
+
+    function set($name, $value){
         $this->vars[$name] = $value;
     }
-    function parse(){
-        $str = $this->content;
-        foreach ($this->varas as $name=>$value) {
-            $str= str_replace('('.$name.')', $value, $str);
 
+    function parse(){
+        $str = $this->content; // malli sisu algväärtus
+        foreach ($this->vars as $name=>$value){
+            $str = str_replace('{'.$name.'}', $value, $str);
         }
         return $str;
     }
