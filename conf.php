9ee75af760d1893dd37c3f4c076624c28e0adf8d
drew-brenet.oispuu <drewoispuu@ikt.khk.ee>
2018-01-23 12:40:01 +0200

    Lisatud kasutusele http klassi fail ja loodud tema objekt

--- conf.php
+++ conf.php
@@ -6,9 +6,12 @@
  * Time: 9:45
  */
 
-// kaustade ja failide konstantsed nimetused
+
 define('MODEL_DIR', 'model/');
 define('VIEW_DIR', 'views/');
 define('CONTROL_DIR', 'controllers/');
 
 require_once MODEL_DIR.'template.php';
+require_once MODEL_DIR.'http.php';
+
+$http = new http();
