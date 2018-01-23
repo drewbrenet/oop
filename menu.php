793558ca1df3d76a8fab41cfb5e406fe45912c2b
drew-brenet.oispuu <drewoispuu@ikt.khk.ee>
2018-01-23 11:08:42 +0200

    Menüü objekt ja menüü elemendi objekt on loodud

---
+++ menu.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: drew-brenet.oispuu
+ * Date: 23.01.2018
+ * Time: 11:08
+ */
+
+$menuTmpl = new template('menu.menu');
+// loome menüü elemendi malli objekti
+$menuItemTmpl = new template('menu.menu_item');
